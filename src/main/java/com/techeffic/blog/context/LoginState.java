569da578ac7c458edb422ce36ae4f7a9a6e3d075
k42jc <k42jc@hotmail.com>
2016-05-05 23:52:32 +0800

    稍稍改了一下样式，简单的修改，却花了一晚的时间，简直是个纠结的人

--- src/main/java/com/techeffic/blog/context/LoginState.java
+++ src/main/java/com/techeffic/blog/context/LoginState.java
@@ -7,8 +7,8 @@ package com.techeffic.blog.context;
  *
  */
 public class LoginState {
-	private String userId;
-	private boolean isLogin;
+	private static String userId;
+	private static boolean isLogin = false;
 	private WebContext webCtx;
 	
 	public LoginState(WebContext webCtx){
